4e330845c2a4150f2ac72ca10530ea52446a68d7
Donald Akite <donaldakite27@gmail.com>
2025-12-07 21:06:11 +0000

    Update CORS configuration to remove unnecessary login path from allowed origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:4200", "https://majestic-haupia-a9adb2.netlify.app", "https://curious-lolly-f1d397.netlify.app/login" # later change to the domain of the frontend app
+    origins "http://localhost:4200", "https://majestic-haupia-a9adb2.netlify.app", "https://curious-lolly-f1d397.netlify.app" # later change to the domain of the frontend app
     resource "*",
              headers: :any,
              methods: %i[get post put patch delete options head],
